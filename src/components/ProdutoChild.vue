bb255740ba9adba62fe7c83fac0d8b9bb0c156f6
alicelanznaster <alicelanznaster@gmail.com>
2026-04-14 16:56:20 -0300

    catálogo (revisão)

---
+++ src/components/ProdutoChild.vue
@@ -0,0 +1,12 @@
+<script setup>
+    defineProps(['nome', 'preco', 'categoria'])
+</script>
+
+<template>
+    <li>
+         {{ nome }} - R$ {{ preco }} ({{ categoria }})
+    </li>
+</template>
+
+<style scoped>
+</style>
